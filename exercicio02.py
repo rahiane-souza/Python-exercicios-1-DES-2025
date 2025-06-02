e4aaf702a0b09e1af9a0b59fe080f09a80197bce
rahiane-souza <rahiane.souza@escola.pr.gov.br>
2025-06-02 14:07:08 +0000

    exercicio02.py

--- exercicio02.py
+++ exercicio02.py
@@ -2,4 +2,13 @@
 # para finalizar três tarefas: X, Y e Z.
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
+tempo1 = int(input("digite o tempo nessesário para a tividade1"))
+tempo2 = int(input("digite o tempo nessesário para a tividade2"))
+tempo3 = int(input("digite o tempo nessesário para a tividade3"))
 
+soma = tempo1 + tempo2 + tempo3
+if tempo1 < 0 or tempo2 < 0 or tempo3 < 0:
+    print ("erro número negativo.")
+else:
+    soma = tempo1 + tempo2 + tempo3
+      print(f"tempo total do projeto:{soma} dias")
