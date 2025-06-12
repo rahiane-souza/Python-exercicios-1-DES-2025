72e6859e7d43a60f955307165bdf4b04e9d100f9
rahiane-souza <rahiane.souza@escola.pr.gov.br>
2025-06-12 12:21:36 +0000

    s

--- exercicio04.py
+++ exercicio04.py
@@ -1,9 +1,14 @@
 # Isabela está desenvolvendo um aplicativo de corrida que calcula a velocidade média do usuário.
 # O programa deve receber a distância percorrida e o tempo gasto, calcular a velocidade e indicar se foi 
 # lenta (<5 km/h), moderada (5 a 10 km/h) ou rápida (>10 km/h).
-distancia= float (int(input("digite a distancia"))
-tempo= float (int(input("digite o tempo"))
-velicidade medias = distancia/tempo
- if velocidade media
-   print
+distancia = int(input("digite a distancia"))
+                   
+tempo = float(input("digite o tempo"))
+              
+velocidade = distancia/tempo
+
+if velocidade < 5:
+   print (" voce foi lento")
+elif velocidade == 5 and velocidade == 10:
+   print("voce foi moderado")
 
